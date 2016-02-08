17808d75bde43fd8610e1bb09b39f527fda8954f
Joe Wang <joewang704@gmail.com>
2016-02-07 21:19:23 -0500

    Add logout functionality to button

--- app/src/main/java/com/example/deeknut/buzzmovie/BMAppActivity.java
+++ app/src/main/java/com/example/deeknut/buzzmovie/BMAppActivity.java
@@ -3,6 +3,8 @@ package com.example.deeknut.buzzmovie;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class BMAppActivity extends AppCompatActivity {
 
@@ -10,10 +12,18 @@ public class BMAppActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app);
+        Button logoutButton = (Button) findViewById(R.id.logout_button);
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                attemptLogout();
+            }
+        });
     }
 
     private void attemptLogout() {
         Intent welcomeScreenIntent = new Intent(this, BMLoginActivity.class);
         startActivity(welcomeScreenIntent);
+        finish();
     }
 }
